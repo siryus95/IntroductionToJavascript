cd0cbc1523c0ef056fc2f050b7476a413c86d096
siryus95 <salaudeenyusuf95@gmail.com>
2022-06-23 17:04:33 +0100

    first javascript commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//MY DETAILS 
+let my_details={
+    First_Name: "Yusuf",
+    Last_Name:"Salaudeen",
+    Height:155 + "cm",
+    Country:"Nigeria", 
+}; 
+document.write(my_details.First_Name + "  " + my_details.Last_Name+",  " + my_details.Height+",  " + my_details.Country)
+console.log(my_details)
+
